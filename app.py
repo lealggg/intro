315374db01bfb8625ba7b123a7f984915ebc2f8a
lealggg <160857577+lealggg@users.noreply.github.com>
2024-02-22 07:42:55 -0500

    Update app.py

--- app.py
+++ app.py
@@ -7,3 +7,17 @@ st.write(" Conoceras facilmente a este personaje ")
 image = Image.open ("leal.jpeg")
 
 st.image(image, caption="leal")
+texto = st.text_input("escribe algo", "este es mi texto")
+st.write("el texto escrito es ", texto)
+
+st.subheader("ahora usemos 2 columnas")
+
+col1, col2 = st.columns(2)
+
+with col1:
+  st.subheader("esta es la primera columna")
+  st.write("leal gg es una gran persona ")
+  resp = st.checkbox("estoy de acuerdo")
+  if resp:
+    st.write("correcto!")
+    
