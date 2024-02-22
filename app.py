094523b8452ac4f77820c876a6f1909c8b5dd428
lealggg <160857577+lealggg@users.noreply.github.com>
2024-02-22 07:23:25 -0500

    Update app.py

--- app.py
+++ app.py
@@ -1,3 +1,7 @@
 import streamlit as st 
 from PIL import Image 
-st.title (" Mi primera app !!")
+st.title (" Leal G!!")
+
+st.header(" En este espacio encontraras informacion sobre leal g ")
+st.write(" Conoceras facilmente a este personaje ")
+image = Image.open ("")
